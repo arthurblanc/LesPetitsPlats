490e3f3f61119122c1ea27b7209d5a425d404c6c
Arthur BLANC <arthur.blanc@outlook.com>
2022-01-04 11:25:46 +0100

    change algorythm for mainSearch()

--- assets/js/functions/mainSearch.js
+++ assets/js/functions/mainSearch.js
@@ -1,16 +1,5 @@
-// Main Search function with array filter()
-const mainSearch = (searchValue) => {
-	const search = searchValue.toLowerCase();
-	recipesFilteredBySearch = recipesFilteredByTags;
-	recipesFilteredBySearch = recipesFilteredBySearch.filter(
-		(recipe) => recipe.name.toLowerCase().includes(search) || recipe.description.toLowerCase().includes(search) || recipe.ingredients.some((el) => el.ingredient.toLowerCase().includes(search))
-	);
-
-	renderRecipesAndTags(recipesFilteredBySearch);
-};
-
 // Main Search function with while/for loop
-/*const mainSearch = (value) => {
+const mainSearch = (value) => {
 	const search = value.toLowerCase();
 	const newRecipesFilterByTags = [];
 	recipesFilteredBySearch = recipesFilteredByTags;
@@ -24,7 +13,7 @@ const mainSearch = (searchValue) => {
 		}
 	}
 	renderRecipesAndTags(newRecipesFilterByTags);
-};*/
+};
 
 const searchbarValue = (e) => {
 	// If searchbar have 3 characters launch search with input else launch empty search
